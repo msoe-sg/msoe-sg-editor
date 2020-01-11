ed9f0dc640f87d203e5f38e5b15de647c06856ea
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-01-11 10:37:03 -0600

    Fixing SessionController Tests

--- test/integration/sessions_controller_test.rb
+++ test/integration/sessions_controller_test.rb
@@ -1,18 +1,15 @@
 class SessionsControllerTest < BaseIntegrationTest
   test 'create should create a user given the omniauth information and store the user in the session' do 
     # Arrange
-    mock_request = Request.new
-    mock_request.env = {
-      "omniauth.auth": {
-        'uid': 1,
-        'info': {
-            'name': 'Andy'
-        }
-      }
-    }
-
-    SessionsController.any_instance.expects(:request).returns(mock_request).at_least_once
-
+    OmniAuth.config.add_mock(:google_oauth2, {
+      'uid': 1,
+      'info': {
+        'name': 'Andy'
+      } 
+    })
+    
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+    
     # Act
     get '/auth/google_oauth2/callback'
 
